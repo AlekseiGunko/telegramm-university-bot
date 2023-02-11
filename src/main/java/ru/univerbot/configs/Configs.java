efddc639b70c88f3b5216840f780b9ce36184242
Aleksei Gunko <igrokk32rus@yandex.ru>
2023-02-12 00:25:52 +0300

    test new class

--- src/main/java/ru/univerbot/configs/Configs.java
+++ src/main/java/ru/univerbot/configs/Configs.java
@@ -1,12 +1,10 @@
-package ru.univerbot.config;
+package ru.univerbot.configs;
 
 import lombok.Getter;
 
 @Getter
-public class Config {
-
+public class Configs {
     private final String BOT_NAME = "UniversityBodroBot";
     private final String BOT_TOKEN = "5976408224:AAEapw9YKJdLp4Uh9lB1mICixSzItmKmss8";
     private final String COMMON_PASSWORD = "BodroCoffeePassword";
-
 }
--- src/main/java/ru/univerbot/core/CoreBot.java
+++ src/main/java/ru/univerbot/core/CoreBot.java
@@ -2,23 +2,25 @@ package ru.univerbot.core;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.univerbot.config.Config;
+import ru.univerbot.Pass;
+import ru.univerbot.configs.Configs;
 import ru.univerbot.service.SendMessageOperationService;
 import ru.univerbot.store.ArrayListStore;
 
 
-
+import static ru.univerbot.Pass.COMMON_PASSWORD;
 import static ru.univerbot.constant.VarConstant.*;
 
 public class CoreBot extends TelegramLongPollingBot {
     SendMessageOperationService operationService = new SendMessageOperationService();
     private ArrayListStore store = new ArrayListStore();
-    private Config config = new Config();
+    private Configs configs = new Configs();
     private boolean startAdd;
     private boolean pass = false;
     private boolean endDel;
     private boolean name_1;
     private boolean name_2;
+    private Pass password = new Pass();
 
 
     @Override
@@ -27,116 +29,115 @@ public class CoreBot extends TelegramLongPollingBot {
         try {
             if (update.hasMessage() && update.getMessage().hasText()) {
 
-            switch (update.getMessage().getText()) {
 
-                case EXAMS:
-                    execute(operationService.createExamsMessage(update));
-                    break;
+                switch (update.getMessage().getText()) {
 
-                case START:
-                    execute(operationService.createHelloMessage(update));
-                    break;
-
-                case COMMON_PASSWORD:
-                    execute(operationService.createTruePassword(update));
-                    pass = true;
-                    break;
-
-                case ADD_TRAINEES:
-                    execute(operationService.createStartMessage(update));
-                    break;
-
-                case ADD_TRAINEE:
-                    startAdd = true;
-                    execute(operationService.createAddMessage(update));
-                    break;
-
-                case END_TRAINEE:
-                    startAdd = false;
-                    execute(operationService.createEndMessage(update));
-                    break;
-
-                case TRAINEE:
-                    if (startAdd == false) {
-                        execute(operationService.createListMessage(update));
-                        execute(operationService.createSimpleMessage(update, store.selectAll()));
+                    case EXAMS:
+                        execute(operationService.createExamsMessage(update));
                         break;
-                    }
 
-                case DELETED_TRAINEE:
-                    endDel = true;
+                    case START:
+                        execute(operationService.createHelloMessage(update));
+                        break;
+
+                    case COMMON_PASSWORD:
+                        execute(operationService.createTruePassword(update));
+                        pass = true;
+                        break;
+
+                    case ADD_TRAINEES:
+                        execute(operationService.createStartMessage(update));
+                        break;
+
+                    case ADD_TRAINEE:
+                        startAdd = true;
+                        execute(operationService.createAddMessage(update));
+                        break;
+
+                    case END_TRAINEE:
+                        startAdd = false;
+                        execute(operationService.createEndMessage(update));
+                        break;
+
+                    case TRAINEE:
+                        if (startAdd == false) {
+                            execute(operationService.createListMessage(update));
+                            execute(operationService.createSimpleMessage(update, store.selectAll()));
+                            break;
+                        }
+
+                    case DELETED_TRAINEE:
+                        endDel = true;
                         execute(operationService.createDeletedMessage(update));
                         break;
 
-                case MAIN:
-                    execute(operationService.mainMonitor(update));
-                    break;
-
-                case LIST_COMMAND:
-                    execute(operationService.listCommand(update));
-                    break;
-
-                case NAME_1:
-                    name_1 = true;
-                    execute(operationService.createName1Message(update));
-                    break;
-
-                case NAME_2:
-                    name_2 = true;
-                    execute(operationService.createName2Message(update));
-                    break;
-
-
-                case NAME_3:
-                    //флаги установить
-                    execute(operationService.createName3Message(update));
-                    break;
-
-                case NAME_4:
-                    //флаги установить
-                    execute(operationService.createName4Message(update));
-                    break;
-
-
-
-                default:
-                   if (startAdd == true && name_1 == true) {
-                        store.save(update.getMessage().getText() + " - " + NAME_1);
+                    case MAIN:
+                        execute(operationService.mainMonitor(update));
                         break;
-                    }
 
-                    if (startAdd == true && name_2 == true) {
-                        store.save(update.getMessage().getText() + " - " + NAME_2);
+                    case LIST_COMMAND:
+                        execute(operationService.listCommand(update));
                         break;
-                    }
 
-                    if (endDel == true) {
-                        store.deleted(update.getMessage().getText());
-                        execute(operationService.createCompletedMessage(update));
-                    }
-                    if (pass == false) {
-                        execute(operationService.createErrorPassword(update));
+                    case NAME_1:
+                        name_1 = true;
+                        execute(operationService.createName1Message(update));
                         break;
-                    }
+
+                    case NAME_2:
+                        name_2 = true;
+                        execute(operationService.createName2Message(update));
+                        break;
+
+
+                    case NAME_3:
+                        //флаги установить
+                        execute(operationService.createName3Message(update));
+                        break;
+
+                    case NAME_4:
+                        //флаги установить
+                        execute(operationService.createName4Message(update));
+                        break;
+
+
+                    default:
+                        if (startAdd == true && name_1 == true) {
+                            store.save(update.getMessage().getText() + " - " + NAME_1);
+                            break;
+                        }
+
+                        if (startAdd == true && name_2 == true) {
+                            store.save(update.getMessage().getText() + " - " + NAME_2);
+                            break;
+                        }
+
+                        if (endDel == true) {
+                            store.deleted(update.getMessage().getText());
+                            execute(operationService.createCompletedMessage(update));
+                        }
+                        if (pass == false) {
+                            execute(operationService.createErrorPassword(update));
+                            break;
+                        }
+
+                }
 
             }
 
-        }
-
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.getMessage();
         }
     }
 
     @Override
     public String getBotUsername() {
-        return config.getBOT_NAME();
+        return configs.getBOT_NAME();
     }
 
     @Override
     public String getBotToken() {
-        return config.getBOT_TOKEN();
+        return configs.getBOT_TOKEN();
     }
 
 }
